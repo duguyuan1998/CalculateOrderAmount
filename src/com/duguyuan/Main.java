82d2d7c377fec3ba8b525cdf1f7fb00e03f35418
duguyuan1998 <1820556468@qq.com>
2021-01-30 10:47:27 +0800

    CalculateOrderAmount code second commit

---
+++ src/com/duguyuan/Main.java
@@ -0,0 +1,23 @@
+package com.duguyuan;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Item huaWei = new Item("C_101", "HUAWEI Mate30 Pro", 5000.00);
+        Item xiaoMi = new Item("C_102", "XIAOMI 10 Pro", 4699.00);
+        Map<String, Item> items = new HashMap<String, Item>();
+        items.put(huaWei.getItemCode(), huaWei);
+        items.put(xiaoMi.getItemCode(), xiaoMi);
+
+        Order order = new Order(items);
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+        String[] datas = input.split(",");
+        double orderAmount = order.getOrderAmount(datas[0], Integer.parseInt(datas[1]));
+        System.out.println(orderAmount);
+    }
+}
